526d468b49c95ca655e77bd04fbe1c1086a9f5c3
Eben <Eben@DESKTOP-18JPE9U>
2023-04-28 22:09:23 +0200

    Add project files.

---
+++ IVS/Pages/Doctors/viewdataDoctors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IVS.Pages.Doctors
+{
+    public class viewdataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ IVS/Pages/insurancepro/viewdataInsurancePro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IVS.Pages.insurance_provider
+{
+    public class viewdataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
